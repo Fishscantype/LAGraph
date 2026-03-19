0f16c29643dfcdb44a59a304d8ddfb278c84c269
Tim Davis <DrTimothyAldenDavis@gmail.com>
2026-03-19 14:32:00 -0500

    testing RMAT

--- experimental/test/test_RMAT.c
+++ experimental/test/test_RMAT.c
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------
-// LAGraph/src/test/test_HelloWorld.c: test cases for LAGraph_HelloWorld
+// LAGraph/src/test/test_RMAT.c: test cases for LAGraph_RMAT
 //----------------------------------------------------------------------------
 
 // LAGraph, (c) 2019-2022 by The LAGraph Contributors, All Rights Reserved.
@@ -13,14 +13,13 @@
 
 //-----------------------------------------------------------------------------
 
-// This is a very simple "hello world" example of a test program for an
-// algorithm in the experimental/algorithm folder.
-
-// TODO: GrB_select with GrB_TRIL to grab the lower triangular matric. Do -1 to get rid of the diagonal to prevent self edges
-// make random matrix wrapper to create random graph, make graph square, then use above step to make symmetric IF directed is false
-// operator for ewise_add does not matter if tril and tril transpose are added, can just pass in grb_fp64 as type
-// operator: GrB_oneb_fp64 for unweighted graph
-// function for removing self edges: LAGrapg_delete_self_edges
+// TODO: GrB_select with GrB_TRIL to grab the lower triangular matric. Do -1 to
+// get rid of the diagonal to prevent self edges make random matrix wrapper to
+// create random graph, make graph square, then use above step to make
+// symmetric IF directed is false operator for ewise_add does not matter if
+// tril and tril transpose are added, can just pass in grb_fp64 as type
+// operator: GrB_oneb_fp64 for unweighted graph function for removing self
+// edges: LAGrapg_delete_self_edges
 
 #include <stdio.h>
 #include <acutest.h>
@@ -203,20 +202,19 @@ void test_RMAT_5 (void)
     // Generate a basic RMAT graph
     //--------------------------------------------------------------------------
 
+    // FIXME: this is too big for the test_RMAT; put in rmat_demo.c in
+    // experimental/benchmark/
+
     GrB_Matrix A = NULL ;
     GrB_Index n = 59 ;           // num of nodes is 2^n
     GrB_Index nedges = 100000000 ;     // number of edges
     double a = 0.45; double b = 0.15; double c = 0.15; double d = 0.25; // RMAT probabilities
     uint64_t seed = 42 ;
 
-    // clock_t start, end;
-    // double cpu_time_used;
-    // start = clock();
-    struct timespec start, end;
-    clock_gettime(CLOCK_MONOTONIC, &start);
+    double start = LAGraph_WallClockTime ( ) ;
     info = LAGraph_RMAT(&A, n, nedges, seed, a, b, c, d, msg);
-    clock_gettime(CLOCK_MONOTONIC, &end);
-    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    double end = LAGraph_WallClockTime ( ) ;
+    double elapsed = end - start ;
     printf("Time taken: %.6f seconds\n", elapsed);
     // printf("LAGraph_RMAT error: %s\n", msg);
     TEST_CHECK(info == GrB_SUCCESS);
@@ -226,8 +224,8 @@ void test_RMAT_5 (void)
     // Print the adjacency matrix
     //--------------------------------------------------------------------------
 
-    // printf("\nAdjacency matrix of RMAT graph:\n");
-    // OK(LAGraph_Matrix_Print(A, LAGraph_COMPLETE, stdout, msg));
+    printf("\nAdjacency matrix of RMAT graph:\n");
+    OK(LAGraph_Matrix_Print(A, LAGraph_SHORT, stdout, msg));
 
     // end = clock();
     // cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
